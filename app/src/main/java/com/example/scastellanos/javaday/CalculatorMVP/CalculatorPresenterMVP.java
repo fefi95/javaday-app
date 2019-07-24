c05e067dab5aec5d496f9d97e3a3e2fe223cf645
Stefani Castellanos <scct95@hotmail.com>
2019-07-24 10:07:49 -0500

    Expression validator

--- app/src/main/java/com/example/scastellanos/javaday/CalculatorMVP/CalculatorPresenterMVP.java
+++ app/src/main/java/com/example/scastellanos/javaday/CalculatorMVP/CalculatorPresenterMVP.java
@@ -1,5 +1,8 @@
 package com.example.scastellanos.javaday.CalculatorMVP;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
@@ -7,6 +10,8 @@ import javax.script.ScriptException;
 public class CalculatorPresenterMVP implements CalculatorContractMVP.Presenter {
 
     CalculatorContractMVP.View mView;
+    private final String mathRegex = "^([-]?\\d+(\\.\\d*)?)([-+/*]\\d+(\\.\\d*)?)*[-+/*]?";
+    private Pattern mMathPtrn = Pattern.compile(mathRegex);
 
     public CalculatorPresenterMVP(CalculatorContractMVP.View view){
         mView = view;
@@ -23,16 +28,16 @@ public class CalculatorPresenterMVP implements CalculatorContractMVP.Presenter {
     }
 
     @Override
-    public void addTextIfValid(String text, String charToAdd){
+    public void addTextIfValid(String text, String charToAdd) {
         String tempExpr = text.concat(charToAdd);
-        if(isValidExpression(tempExpr)){
+        if(isValidExpression(tempExpr)) {
             mView.updateInputText(tempExpr);
         }
     }
 
-    private Boolean isValidExpression(CharSequence expr){
-        // TODO: implement this
-        return true;
+    private Boolean isValidExpression(CharSequence expr) {
+        Matcher m = mMathPtrn.matcher(expr);
+        return m.matches();
     }
 
     private Double evalExpression(CharSequence text) throws ScriptException {
--- app/src/test/java/com/example/scastellanos/javaday/CalculatorPresenterMVPTest.java
+++ app/src/test/java/com/example/scastellanos/javaday/CalculatorPresenterMVPTest.java
@@ -4,7 +4,6 @@ import com.example.scastellanos.javaday.CalculatorMVP.CalculatorContractMVP;
 import com.example.scastellanos.javaday.CalculatorMVP.CalculatorPresenterMVP;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -25,17 +24,57 @@ public class CalculatorPresenterMVPTest {
         presenterMVP = new CalculatorPresenterMVP(activityMVP);
     }
 
+    // Tests for addTextIfValid function
+
     @Test
-    public void addTextIfValid_validExpr() {
+    public void addTextIfValid_valid_simpleExpr() {
         presenterMVP.addTextIfValid("3+", "4");
         verify(activityMVP, times(1)).updateInputText("3+4");
     }
 
-    @Ignore
-    public void addTextIfValid_notValidExpr() {
-        presenterMVP.addTextIfValid("3+", "+");
-        verify(activityMVP, times(1)).updateInputText("3+");
+    @Test
+    public void addTextIfValid_valid_DecimalExpr() {
+        presenterMVP.addTextIfValid("3", ".");
+        verify(activityMVP, times(1)).updateInputText("3.");
     }
+
+    @Test
+    public void addTextIfValid_notValid_ExprPlus() {
+        presenterMVP.addTextIfValid("3+", "+");
+        verify(activityMVP, times(0)).updateInputText("3++");
+    }
+
+    @Test
+    public void addTextIfValid_notValid_ExprMinus() {
+        presenterMVP.addTextIfValid("3+", "-");
+        verify(activityMVP, times(0)).updateInputText("3+-");
+    }
+
+    @Test
+    public void addTextIfValid_notValid_ExprMul() {
+        presenterMVP.addTextIfValid("3+", "*");
+        verify(activityMVP, times(0)).updateInputText("3+*");
+    }
+
+    @Test
+    public void addTextIfValid_notValid_ExprDiv() {
+        presenterMVP.addTextIfValid("3+", "/");
+        verify(activityMVP, times(0)).updateInputText("3+/");
+    }
+
+    @Test
+    public void addTextIfValid_notValid_ExprDot() {
+        presenterMVP.addTextIfValid("3+", ".");
+        verify(activityMVP, times(0)).updateInputText("3+.");
+    }
+
+    @Test
+    public void addTextIfValid_notValid_DecimalExpr() {
+        presenterMVP.addTextIfValid("3.", ".");
+        verify(activityMVP, times(0)).updateInputText("3..");
+    }
+
+    // Tests for calculateResult function
 
     @Test
     public void calculateResult_validExpr(){
