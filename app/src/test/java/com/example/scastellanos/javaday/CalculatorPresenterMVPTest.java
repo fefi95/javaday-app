39953c6ed4bb1346b308da294d2a44b8e89819c5
Stefani Castellanos <scct95@hotmail.com>
2019-08-12 07:43:13 -0500

    Presentation

--- app/src/test/java/com/example/scastellanos/javaday/CalculatorPresenterMVPTest.java
+++ app/src/test/java/com/example/scastellanos/javaday/CalculatorPresenterMVPTest.java
@@ -24,6 +24,7 @@ public class CalculatorPresenterMVPTest {
 
     private CalculatorPresenterMVP presenterMVP;
     private MockWebServer server;
+
     @Rule
     public TrampolineSchedulerRule rule = new TrampolineSchedulerRule();
 
